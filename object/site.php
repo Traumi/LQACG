5857912deffc66691c5ac11fa16f319303b51b9d
Traumi <brsjdtendo@gmail.com>
2019-04-05 16:31:55 +0200

    Update site.php

--- object/site.php
+++ object/site.php
@@ -4,7 +4,7 @@
         public function __construct() {
             $this->version = "0.1";
             $this->riotPatch = "9.6.1";
-            $this->key = "RGAPI-da466fbb-55c2-2360-f933-143e13f229de";
+            $this->key = "key";
         }
     }
     
