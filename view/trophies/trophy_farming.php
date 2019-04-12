4b62fb096069bcf5e865741b2bb3d2a6fccb4e18
brosaz <brosaz@osaxis.fr>
2019-04-12 14:28:28 +0200

    Tower icon

--- view/trophies/trophy_farming.php
+++ view/trophies/trophy_farming.php
@@ -40,7 +40,7 @@
 <div class="col-2  <?php echo $level; ?>">
     <svg viewbox="0 0 1000 1000" style="width:100%;">
         <circle cx="500" cy="400" r="390" stroke="black" stroke-width="10" fill="transparent"/>
-        <!--<image xlink:href="./images/minions/<?php echo $level; ?>.png" x="100" y="50" height="450" width="800" /> -->
+        <image xlink:href="./images/tower/<?php echo $level; ?>.png" x="100" y="50" height="450" width="800" />
         <text x="500" y="625" text-anchor="middle" font-weight="800" font-size="125"><?php echo $level_value; ?></text>
         <text x="500" y="725" text-anchor="middle" font-weight="600" font-size="75"><?php echo $lol_profil['TOWER']; ?></text>
         <text x="500" y="900" text-anchor="middle" font-weight="600" font-size="100">Tower : <?php echo $nlvl ?></text>
